6b08cc346331dea8835977bf58561b95c507096a
Cameron Lowell Palmer <cameron.palmer@shortcut.no>
2014-05-27 20:49:26 +0200

    Updated the build number, added in Hockey and New Relic and update the Podfile

--- Fantorangen/WBSAppDelegate.m
+++ Fantorangen/WBSAppDelegate.m
@@ -9,11 +9,11 @@
 #import "WBSAppDelegate.h"
 
 #import <AVFoundation/AVFoundation.h>
-#import <TestFlightSDK/TestFlight.h>
+#import <HockeySDK/HockeySDK.h>
 #import <NewRelicAgent/NewRelic.h>
 
-static NSString *const kTestFlightSDKKey = @"0970bc26-64a6-4a0e-9a6d-ae9a223bd210";
-static NSString *const kNewRelicSDKKey = @"AAec230ed311d007bb190cb88a5e5ab2afcbff61e5";
+static NSString *const kHockeySDKBetaKey = @"960b7c9d44b074ccb2b518fe87a8f5b7";
+static NSString *const kNewRelicSDKKey = @"AAc5e4ecfc6e24d94b50e23eba991902aa6df41435";
 
 
 
@@ -21,12 +21,17 @@ static NSString *const kNewRelicSDKKey = @"AAec230ed311d007bb190cb88a5e5ab2afcbf
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
+
     [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
     
     [DDLog addLogger:[DDASLLogger sharedInstance]];
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
+
+    [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:kHockeySDKBetaKey];
+    [[BITHockeyManager sharedHockeyManager] startManager];
+    [[BITHockeyManager sharedHockeyManager].authenticator authenticateInstallation];
     
-    [TestFlight takeOff:kTestFlightSDKKey];
     [NewRelicAgent startWithApplicationToken:kNewRelicSDKKey];
     
     return YES;
@@ -34,29 +39,28 @@ static NSString *const kNewRelicSDKKey = @"AAec230ed311d007bb190cb88a5e5ab2afcbf
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    DDLogVerbose(@"%s", __PRETTY_FUNCTION__);
 }
 
 @end
--- Podfile
+++ Podfile
@@ -1,9 +1,10 @@
 platform :ios, '7.0'
 
-pod 'Reveal-iOS-SDK'
-pod 'AFNetworking'
-pod 'hpple'
+pod 'AFNetworking', '~> 2.2.4'
+pod 'hpple', '~> 0.2.0'
 
-pod 'TestFlightSDK'
-pod 'NewRelicAgent'
-pod 'CocoaLumberjack'
+pod 'HockeySDK', '~> 3.5.5'
+pod 'NewRelicAgent', '~> 3.324'
+pod 'CocoaLumberjack', '~> 1.9.0'
+
+pod 'Reveal-iOS-SDK', '~> 1.0.4'
